ec01024ef4ef659f40dc99eeedf115c38605b884
harshlowanshi <harshlowanshi@gmail.com>
2024-12-14 16:20:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+const mainContainer= document.getElementById("mainContainer")
+mainContainer.style.height="100vh"
+mainContainer.style.width="100%"
+mainContainer.style.display="flex"
+mainContainer.style.alignItems="center"
+mainContainer.style.justifyContent="center"
+mainContainer.style.backgroundColor="#B1AFAF"
+console.log(mainContainer);
+
+
+
+const card=document.getElementById("card")
+card.style.width="30%"
+card.style.height="70%"
+card.style.borderRadius="1em"
+card.style.backgroundColor="#404E57"
+card.style.color="white"
+card.style.display="flex"
+card.style.alignItems="center"
+card.style.justifyContent="center"
+// card.style.paddingBlock="1rem"
+card.style.gap=""
+card.style.flexDirection=" column"
+card.style.border="3.5px solid #C14181"
+// card.style.paddingTop="10em"
+
+console.log(card);
+
+
+
+const profilePic=document.getElementById("profilePic")
+profilePic.style.width="40%"
+profilePic.style.height="32%"
+profilePic.style.border="3px solid #C14181"
+profilePic.style.borderRadius="100%"
+// profilePic.style.border="5px solid #C14181"
+
+
+console.log(profilePic);
+
+
+
+const pic=document.getElementsByTagName("mainPic")
+mainPic.style.width="100%"
+mainPic.style.height="100%"
+mainPic.style.borderRadius="100%"
+
+console.log(pic);
+
+
+const head=document.getElementById("head")
+head.style.fontSize="1.6em"
+
+console.log(head);
+
+
+
+const icon=document.getElementById("icon")
+icon.style.width="100%"
+icon.style.display="flex"
+icon.style.alignItems="center"
+icon.style.justifyContent="center"
+icon.style.gap="1rem"
+icon.style.fontSize="1.6rem"
+
+console.log(icon);
+
+const btn =document.getElementById("btn")
+btn.style.width="50%"
+btn.style.height="10%"
+btn.style.borderRadius="5em"
+btn.style.fontSize="1rem"
+btn.style.backgroundColor=" #C14181"
+console.log(btn);
